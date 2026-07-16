0a8b658e1239faf9ba12f5a782281230c7f46564
augustelke <auguste.marcorelles.5@gmail.com>
2026-07-16 21:40:14 +0200

    Update clicker.py

--- src/clicker.py
+++ src/clicker.py
@@ -1,1 +1,30 @@
+import pyautogui
+import time
+import threading
 
+from src.settings import CLICK_INTERVAL
+
+
+class AutoClicker:
+
+    def __init__(self):
+        self.running = False
+        self.thread = None
+
+    def click_loop(self):
+        while self.running:
+            pyautogui.click()
+            time.sleep(CLICK_INTERVAL)
+
+    def start(self):
+        if not self.running:
+            self.running = True
+
+            self.thread = threading.Thread(
+                target=self.click_loop
+            )
+
+            self.thread.start()
+
+    def stop(self):
+        self.running = False
